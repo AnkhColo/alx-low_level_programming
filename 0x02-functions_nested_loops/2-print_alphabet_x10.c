1f667ec378499fa1894a77574b1ab7da792233c1
AnkhColo <muriithicolin98@gmail.com>
2022-10-18 15:34:56 -0700

    yes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times and adds a new line
+ */
+void print_alphabet_x10(void);
+{
+	char (j);
+
+	for (j = 1; j <= 10; j++)
+		_putchar(j);
+
+	_putchar('\n');
+	{
+
+	char (xy);
+
+	for (xy = 'a'; xy <= 'z'; xy++)
+		putchar(xy);
+	}
+
+	_putchar('\n')
+}
+
