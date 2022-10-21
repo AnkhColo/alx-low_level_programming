e1a7b443eccafb0e63caebafa551f8da65c22c39
AnkhColo <muriithicolin98@gmail.com>
2022-10-21 07:15:02 -0700

    Sorted

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
 /**
- *
- * * main - causes an infinite loop
- *
- * * Return: 0
+ * main - causes an infinite loop
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
-	print("Infinite Loop incoming :(\n");
+	printf("Infinite Loop incoming :(\n");
 
-	i=0;
+	i = 0;
 
-	while (i < 10) /* The infinte loop is caused here because i does not increment */
-	{ 
-		putchar (i);
+	while (i < 10) /* The infinte loop is caused here - i does not increment */
+	{
+		putchar(i);
 	}
 
-	print("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,16 @@
+This project is about debugging.
+
+The rubber duck debugging method is very effective for this.
+
+The method is summarized as:
+Get stuck writing code
+
+Get your rubber duck and explain the overall process or aim of your code
+
+Explain the code line by line and its function
+
+Notice the error made!
+
+You've identified the error, fix it! 
+
+You're done! Congratulate yourself!
